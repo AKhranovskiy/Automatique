8d90fbb552b8aa8b1dacd63d580bfe9d9a7f0e7b
Andrey Khranovsky <akhranovskiy@gmail.com>
2018-09-22 22:48:27 +0200

    Extract position

--- main.cpp
+++ main.cpp
@@ -1,81 +1,11 @@
-#include "version.h"
 #include "cxhash.hpp"
+#include "position.hpp"
+#include "version.h"
 
-#include <array>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-
-
-enum class EDirection {
-  North,
-  East,
-  South,
-  West
-};
-
-using coord_t = size_t;
-
-template <coord_t W, coord_t H>
-struct position_t {
-  coord_t x{0};
-  coord_t y{0};
-
-  position_t() = default;
-
-  position_t(coord_t x, coord_t y) noexcept
-      : x{x % W}
-      , y{y % H}
-  {
-  }
-
-  position_t(const position_t& pos) noexcept
-      : x{pos.x}
-      , y{pos.y}
-  {
-  }
-  position_t(position_t&& pos) noexcept
-      : x{pos.x}
-      , y{pos.y}
-  {
-  }
-
-  position_t& operator=(const position_t& pos) noexcept
-  {
-    x = pos.x;
-    y = pos.y;
-    return *this;
-  }
-  position_t move(EDirection dir) const noexcept
-  {
-    switch (dir) {
-    case EDirection::North:
-      return position_t{x, y > 0 ? y - 1 : H - 1};
-    case EDirection::East:
-      return position_t{x + 1, y};
-    case EDirection::South:
-      return position_t{x, y + 1};
-    case EDirection::West:
-      return position_t{x > 0 ? x - 1 : W - 1, y};
-    }
-  }
-
-  struct Hash {
-    std::size_t operator()(const position_t& p) const noexcept
-    {
-      return cx::FNVhash(p.x, p.y);
-    }
-  };
-
-  bool operator==(const position_t& o) const noexcept
-  {
-    return x == o.x && y == o.y;
-  }
-};
 
 template <coord_t W, coord_t H>
 std::ostream& operator<<(std::ostream& os, const position_t<W, H>& pos)
@@ -83,134 +13,20 @@ std::ostream& operator<<(std::ostream& os, const position_t<W, H>& pos)
   return (os << '(' << pos.x << ',' << pos.y << ')');
 }
 
-template <coord_t W, coord_t H>
-struct world_t {
-  using position_t = position_t<W, H>;
-
-  using distance_t = size_t;
-  distance_t distance(const position_t& from, const position_t& to) const
-  {
-    const auto dst = [](auto a, auto b) {
-      return a > b ? a - b : b - a;
-    };
-
-    auto dx = std::min(dst(from.x, to.x), dst(from.x + W, to.x));
-    auto dy = std::min(dst(from.y, to.y), dst(from.y + H, to.y));
-    return dx * dx + dy * dy;
-  }
-
-  std::array<position_t, 4> neighborhs(const position_t& pos) const
-  {
-    return {pos.move(EDirection::North), pos.move(EDirection::East), pos.move(EDirection::South), pos.move(EDirection::West)};
-  }
-
-  std::vector<position_t> findPath(position_t start, position_t goal) const
-  {
-    using set_t = std::unordered_set<position_t, typename position_t::Hash>;
-    using map_t = std::unordered_map<position_t, distance_t, typename position_t::Hash>;
-
-    // The set of nodes already evaluated
-    set_t closedSet{};
-
-    // The set of currently discovered nodes that are not evaluated yet.
-    // Initially, only the start node is known.
-    set_t openSet;
-    openSet.insert(start);
-
-    // For each node, which node it can most efficiently be reached from.
-    // If a node can be reached from many nodes, cameFrom will eventually contain the
-    // most efficient previous step.
-    std::unordered_map<position_t, position_t, typename position_t::Hash> cameFrom{};
-
-    const auto Inf = W * W + H * H + 1;
-
-    const auto getScore = [&](map_t& s, const position_t& pos) {
-      return s.count(pos) ? s[pos] : Inf;
-    };
-
-    // For each node, the cost of getting from the start node to that node.
-    map_t gScore{};
-
-    // The cost of going from start to start is zero.
-    gScore[start] = 0;
-
-    // For each node, the total cost of getting from the start node to the goal
-    // by passing by that node. That value is partly known, partly heuristic.
-    map_t fScore{};
-
-    // For the first node, that value is completely heuristic.
-    fScore[start] = distance(start, goal);
-
-    const auto findCurrent = [&]() -> position_t {
-      auto score = Inf;
-      const position_t* pos = nullptr;
-      for (const auto& node : openSet) {
-        auto f = getScore(fScore, node);
-        if (f < score) {
-          score = f;
-          pos = &node;
-        }
-      }
-      assert(pos != nullptr);
-      return position_t{*pos};
-    };
-
-    const auto reconstruct_path = [&](const position_t& pos) {
-      auto p = pos;
-      std::vector<position_t> total_path;
-      total_path.push_back(p);
-      while (cameFrom.count(p)) {
-        p = cameFrom[p];
-        total_path.push_back(p);
-      }
-      return total_path;
-    };
-    while (!openSet.empty()) {
-      auto current = findCurrent();
-      if (current == goal) {
-        return reconstruct_path(current);
-      }
-      openSet.erase(current);
-      closedSet.insert(current);
-
-      for (const auto& neighbor : neighborhs(current)) {
-        if (closedSet.count(neighbor)) {
-          continue;
-        }
-
-        // The distance from start to a neighbor
-        auto tentative_gScore = getScore(gScore, current) + distance(current, neighbor);
-
-        if (!openSet.count(neighbor)) // Discover a new node
-          openSet.insert(neighbor);
-        else if (tentative_gScore >= getScore(gScore, neighbor))
-          continue; // This is not a better path.
-
-        // This path is the best until now. Record it!
-        cameFrom[neighbor] = current;
-        gScore[neighbor] = tentative_gScore;
-        fScore[neighbor] = getScore(gScore, neighbor) + distance(neighbor, goal);
-      }
-    }
-    return {};
-  }
-
-private:
-};
+constexpr const coord_t WIDTH{5u};
+constexpr const coord_t HEIGHT{5u};
+using pos_t = position_t<WIDTH, HEIGHT>;
 
 int main()
 {
   std::cout << KVersion << '\n';
   std::cout << "Start game loop" << std::endl;
 
-  const auto world = world_t<5, 5>{};
-  using pos_t = decltype(world)::position_t;
+  assert(2 == distance(pos_t{0, 0}, pos_t{1, 1}));
+  assert(2 == distance(pos_t{0, 0}, pos_t{4, 4}));
+  assert(1 == distance(pos_t{0, 0}, pos_t{4, 0}));
 
-  assert(2 == world.distance(pos_t{0, 0}, pos_t{1, 1}));
-  assert(2 == world.distance(pos_t{0, 0}, pos_t{4, 4}));
-  assert(1 == world.distance(pos_t{0, 0}, pos_t{4, 0}));
-
-  for (const auto& p : world.findPath(pos_t{0, 0}, pos_t{4, 4})) {
+  for (const auto& p : findPath(pos_t{0, 0}, pos_t{4, 4})) {
     std::cout << p;
   }
   // bool quitRequested = false;
---
+++ position.hpp
@@ -0,0 +1,210 @@
+#pragma once
+
+#include "cxhash.hpp"
+
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+enum class EDirection {
+  North,
+  East,
+  South,
+  West
+};
+
+using coord_t = std::size_t;
+
+template <coord_t W, coord_t H>
+struct position_t {
+  static_assert(W > 2, "Width is too small");
+  static_assert(H > 2, "Height is too small");
+
+  coord_t x{0};
+  coord_t y{0};
+
+  constexpr position_t() noexcept = default;
+  constexpr position_t(const position_t& pos) noexcept = default;
+  constexpr position_t(position_t&& pos) noexcept = default;
+
+  constexpr position_t(coord_t x, coord_t y) noexcept
+      : x{x % W}
+      , y{y % H}
+  {
+  }
+
+  constexpr position_t& operator=(const position_t& pos) noexcept = default;
+  constexpr position_t& operator=(position_t&& pos) noexcept = default;
+};
+
+// TODO Spaceship operator?
+template <coord_t W, coord_t H, typename pos_t = position_t<W, H>>
+constexpr bool operator==(const position_t<W, H>& lhs, const pos_t& rhs) noexcept
+{
+  return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+template <coord_t W, coord_t H, typename pos_t = position_t<W, H>>
+constexpr bool operator!=(const position_t<W, H>& lhs, const pos_t& rhs) noexcept
+{
+  return !(lhs == rhs);
+}
+template <coord_t W, coord_t H, typename pos_t = position_t<W, H>>
+constexpr bool operator<(const position_t<W, H>& lhs, const pos_t& rhs) noexcept
+{
+  return lhs.x < rhs.x && lhs.y < rhs.y;
+}
+template <coord_t W, coord_t H, typename pos_t = position_t<W, H>>
+constexpr bool operator>(const position_t<W, H>& lhs, const pos_t& rhs) noexcept
+{
+  return lhs.x > rhs.x && lhs.y > rhs.y;
+}
+
+template <coord_t W, coord_t H, typename pos_t = position_t<W, H>>
+constexpr pos_t move(const position_t<W, H>& pos, EDirection dir) noexcept
+{
+  switch (dir) {
+  case EDirection::North:
+    return {pos.x, pos.y > 0 ? pos.y - 1 : H - 1};
+  case EDirection::East:
+    return {pos.x + 1, pos.y};
+  case EDirection::South:
+    return {pos.x, pos.y + 1};
+  case EDirection::West:
+    return {pos.x > 0 ? pos.x - 1 : W - 1, pos.y};
+  }
+}
+
+using distance_t = decltype(std::declval<coord_t>() * std::declval<coord_t>());
+
+template <coord_t W, coord_t H, typename pos_t = position_t<W, H>>
+constexpr distance_t distance(const position_t<W, H>& from, const pos_t& to) noexcept
+{
+  const auto dst = [](auto a, auto b) {
+    return a > b ? a - b : b - a;
+  };
+
+  auto dx = std::min(dst(from.x, to.x), dst(from.x + W, to.x));
+  auto dy = std::min(dst(from.y, to.y), dst(from.y + H, to.y));
+  return dx * dx + dy * dy;
+}
+
+template <coord_t W, coord_t H>
+constexpr std::array<position_t<W, H>, 4> neighborhs(const position_t<W, H>& pos) noexcept
+{
+  return {
+      move(pos, EDirection::North),
+      move(pos, EDirection::East),
+      move(pos, EDirection::South),
+      move(pos, EDirection::West)};
+}
+
+template <coord_t W, coord_t H, typename pos_t = position_t<W, H>>
+std::vector<pos_t> findPath(const position_t<W, H>& start, const pos_t& goal) noexcept
+{
+  struct Hash {
+    std::size_t operator()(const pos_t& p) const noexcept
+    {
+      return cx::FNVhash(p.x, p.y);
+    }
+  };
+
+  using set_t = std::unordered_set<pos_t, Hash>;
+  using score_map_t = std::unordered_map<pos_t, distance_t, Hash>;
+  using came_from_map_t = std::unordered_map<pos_t, pos_t, Hash>;
+
+  const auto Inf = W * W + H * H + 1;
+
+  // For non-existing key, map creates default value.
+  // But it should be Inf by default.
+  // I return Inf instead of default value to not fill entire map with all possible keys.
+  const auto getScore = [&Inf](score_map_t& score_map, const pos_t& pos) -> distance_t {
+    return score_map.count(pos) ? score_map[pos] : Inf;
+  };
+
+  // The set of nodes already evaluated
+  set_t closedSet{};
+
+  // The set of currently discovered nodes that are not evaluated yet.
+  // Initially, only the start node is known.
+  set_t openSet;
+  openSet.insert(start);
+
+  // For each node, which node it can most efficiently be reached from.
+  // If a node can be reached from many nodes, cameFrom will eventually contain the
+  // most efficient previous step.
+  came_from_map_t cameFrom{};
+
+  // For each node, the cost of getting from the start node to that node.
+  score_map_t gScore{};
+
+  // The cost of going from start to start is zero.
+  gScore[start] = 0;
+
+  // For each node, the total cost of getting from the start node to the goal
+  // by passing by that node. That value is partly known, partly heuristic.
+  score_map_t fScore{};
+
+  // For the first node, that value is completely heuristic.
+  fScore[start] = distance(start, goal);
+
+  // Find node in openSet with minimal fScore
+  const auto findCurrent = [&]() -> pos_t {
+    auto score = Inf;
+    const pos_t* pos = nullptr;
+    for (const auto& node : openSet) {
+      auto f = getScore(fScore, node);
+      if (f < score) {
+        score = f;
+        pos = &node;
+      }
+    }
+    assert(pos != nullptr);
+    return pos_t{*pos};
+  };
+
+  const auto reconstruct_path = [&](const pos_t& pos) {
+    auto p = pos;
+    std::vector<pos_t> total_path;
+    total_path.push_back(p);
+    while (cameFrom.count(p)) {
+      p = cameFrom[p];
+      total_path.push_back(p);
+    }
+    return total_path;
+  };
+
+  while (!openSet.empty()) {
+    auto current = findCurrent();
+    if (current == goal) {
+      return reconstruct_path(current);
+    }
+
+    openSet.erase(current);
+    closedSet.insert(current);
+
+    for (const auto& neighbor : neighborhs(current)) {
+      if (closedSet.count(neighbor)) {
+        continue;
+      }
+
+      // The distance from start to a neighbor
+      auto tentative_gScore = getScore(gScore, current) + distance(current, neighbor);
+
+      if (!openSet.count(neighbor)) { // Discover a new node
+        openSet.insert(neighbor);
+      } else if (tentative_gScore >= getScore(gScore, neighbor)) {
+        continue; // This is not a better path.
+      }
+
+      // This path is the best until now. Record it!
+      cameFrom[neighbor] = current;
+      gScore[neighbor] = tentative_gScore;
+      fScore[neighbor] = getScore(gScore, neighbor) + distance(neighbor, goal);
+    }
+  }
+  return {};
+}
