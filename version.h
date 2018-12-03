b591913242a6ed6e29f96b02b0237b2e63f9419e
Andrey Khranovsky <akhranovskiy@gmail.com>
2018-12-03 05:21:10 +0100

    [version] 0.6/WorldChronicles
    WorldChronicles begins the history management of Automatique world.
    It is a world-wide logger that gives unified format of logs, allows simple customization.

--- version.h
+++ version.h
@@ -11,6 +11,6 @@ struct version_t {
   std::size_t date;
   const char* const name;
 };
-static const version_t KVersion = {0, 5, cx::date(), "Discovery"};
+static const version_t KVersion = {0, 6, cx::date(), "WorldChronicles"};
 
 std::ostream& operator<<(std::ostream& os, const version_t& v) noexcept;
